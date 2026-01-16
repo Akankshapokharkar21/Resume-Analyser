575c588dfd46f3b585dec399d0b60fa0dfac701f
Akanksha Pokharkar <akankshapokharkar21@gmail.com>
2026-01-17 00:23:24 +0530

    Update firebase.jsx

--- mern_ai/src/utils/firebase.jsx
+++ mern_ai/src/utils/firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCljpHQ_cNB7wxVBD91ciuDr_QRRCiqctw",
-  authDomain: "mernai-f65d8.firebaseapp.com",
-  projectId: "mernai-f65d8",
-  storageBucket: "mernai-f65d8.firebasestorage.app",
-  messagingSenderId: "776426239772",
-  appId: "1:776426239772:web:ac63286722aa09c9a06cd9",
-  measurementId: "G-TSK8P1MDKR"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
